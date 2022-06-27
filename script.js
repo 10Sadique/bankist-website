233495fe8780547d1ec1dd0178cdb84d855252a3
10Sadique <jafarsjahan@gmail.com>
2022-06-27 21:31:37 +0600

    page smooth navigation added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict'
 
-///////////////////////////////////////
-//////////   Modal window   ///////////
-///////////////////////////////////////
-
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+///////////////////////////////////////
+//////////   Modal window   ///////////
+///////////////////////////////////////
 
 const openModal = function (e) {
     e.preventDefault()
@@ -35,11 +38,60 @@ document.addEventListener('keydown', function (e) {
 /////////   Smooth Scrolling    //////////
 //////////////////////////////////////////
 
-const btnScrollTo = document.querySelector('.btn--scroll-to')
-const section1 = document.querySelector('#section--1')
-
 btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect()
+    // console.log(s1coords)
+
+    // console.log(e.target.getBoundingClientRect())
+
+    // console.log(
+    //     'Current scroll point (X/Y)',
+    //     window.pageXOffset,
+    //     window.pageYOffset
+    // )
+
+    // Scrolling
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset,
+    //     s1coords.top + window.pageYOffset
+    // )
+
+    // Smooth Scrolling
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth'
+    // })
+
     // Modern Way of Smooth Scrolling
-    section1.scrollIntoView({ behavior: 'smooth'})
+    section1.scrollIntoView({ behavior: 'smooth' })
 })
 
+//////////////////////////////////////////
+/////////   Page Navigation    ///////////
+//////////////////////////////////////////
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         e.preventDefault()
+//         const id = this.getAttribute('href')
+//         console.log(id)
+//         document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+//     })
+// }) 
+
+// This is very unefficient way of Event Handling... Beacuse for n elements there will be n number of call-back function genrated. This problem can be solved by Event Delegation
+
+// Event Delegation Steps:
+// 1. Add Event listener to the common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    // Matching Strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href')
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+})
