e35f0b4f9551f0dfc57e2a6c4d78df95bedc3dec
10Sadique <jafarsjahan@gmail.com>
2022-06-29 23:18:26 +0600

    tab content and menu fade on hover implemented

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
 const btnScrollTo = document.querySelector('.btn--scroll-to')
 const section1 = document.querySelector('#section--1')
 
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+const nav = document.querySelector('.nav')
+
+
 ///////////////////////////////////////
 //////////   Modal window   ///////////
 ///////////////////////////////////////
@@ -95,3 +102,48 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
         document.querySelector(id).scrollIntoView({behavior: 'smooth'})
     }
 })
+
+//////////////////////////////////////////
+///////     Tabbed Components    /////////
+//////////////////////////////////////////
+
+tabsContainer.addEventListener('click', function (e) {
+    let clicked = e.target.closest('.operations__tab')
+
+    // Guard clause
+    if (!clicked) return
+
+    // Removing Active Classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'))
+    tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+    
+    // Active Tab
+    clicked.classList.add('operations__tab--active')
+
+    // Active Content Area
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
+
+//////////////////////////////////////////
+//////     Menu Fade Animation    ////////
+//////////////////////////////////////////
+
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+        const logo = link.closest('.nav').querySelector('img')
+        
+        siblings.forEach(el => {
+            if (el !== link) {
+                el.style.opacity = this
+            }
+        })
+        logo.style.opacity = this
+        // here the this keyword is the argument opactiy
+    }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+
+nav.addEventListener('mouseout', handleHover.bind(1))
